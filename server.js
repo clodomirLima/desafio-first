63bd6204bf1cca2e8e532e7998c9f1c8b13d7e7c
Your Name <nreis787@gmail.com>
2023-10-20 09:45:59 -0300

    refactor

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require("./api/localApi/app");
+const app = require('./src/routes/routes');
 const port = normalizaPort(process.env.PORT || 3000);
 function normalizaPort(val) {
     const port = parseInt(val, 10);
